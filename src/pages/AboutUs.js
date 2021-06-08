a2ed1fc78eafb9c4541b1c1e869c3fbc57afc694
sillyD <zlidio98@gmail.com>
2021-06-08 18:25:34 +0200

    3. Services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,15 @@
 import React from 'react';
 // page components
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs = () => {
      return(
-          <AboutSection />
+          // everything has to have a parent div, that's why we put empty tags(<> </>)
+          <>
+               <AboutSection />
+               <ServicesSection />
+          </>
      )
 }
 
