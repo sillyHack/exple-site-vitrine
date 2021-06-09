876af9c988fd3edce795ac5ce075815b2b9eda3d
sillyD <zlidio98@gmail.com>
2021-06-10 00:57:52 +0200

    5. intro to styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+// import global style
+import GlobalStyle from './components/GlobalStyle';
 // import pages
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs/>
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,32 +1,63 @@
 import React from 'react';
 import home1 from '../img/home1.png';
+// styled
+import styled from 'styled-components';
 
 const AboutSection = () => {
      return(
-        <div>
-             <div className="description">
+        <About>
+             <Description>
                   <div className="title">
-                       <div className="hide">
+                       <Hide>
                             <h2>We work to make</h2>
-                       </div>
-                       <div className="hide">
+                       </Hide>
+                       <Hide>
                             <h2>your <span>dreams</span> comes</h2>
-                       </div>
-                       <div className="hide">
+                       </Hide>
+                       <Hide>
                             <h2>true.</h2>
-                       </div>
+                       </Hide>
                   </div>
                   <p>
                          Contact us for any photography or videography idea that you have.
                          We have professionals with amazing skills.
                   </p>
                   <button>Contact us</button>
-             </div>
-             <div className="image">
+             </Description>
+             <Image>
                   <img src={home1} alt="guy with a camera"/>
-             </div>
-        </div>
+             </Image>
+        </About>
      );
 };
+
+// styled components
+const About = styled.div`
+     min-height: 90vh;
+     display: flex;
+     align-items: center;
+     justify-content: space-between;
+     padding: 5rem 10rem;
+     color: white;
+`
+const Description = styled.div`
+     flex: 1;
+     padding-right: 5rem;
+     h2{
+          font-weight: lighter;
+     }
+`
+const Image = styled.div`
+     flex: 1;
+     overflow: hidden; // the image size changes when the window size changes
+     img{
+          width: 100%;
+          height: 80vh;
+          object-fit: cover; // keep the ratio of the image according to the width and height parameters
+     }
+`
+const Hide = styled.div`
+     overflow: hidden;
+`
 
 export default AboutSection;
