7d9e5566a1bbfd2dc5c6a673a4277f9d51327575
sillyD <zlidio98@gmail.com>
2021-06-15 04:30:14 +0200

    12. Framer motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,24 +1,52 @@
 import React from 'react';
 import home1 from '../img/home1.png';
-// styled
-import styled from 'styled-components';
 import {About, Description, Image, Hide} from '../styles';
+// framer motion
+import {motion} from 'framer-motion';
 
 const AboutSection = () => {
+
+     const titleAnim = {
+          hidden: {opacity: 0},
+          show: {
+               opacity: 1,
+               transition: {
+                    duration: 1
+               }
+          }
+     }
+     const container = {
+          hidden: {x: 100},
+          show: {
+               x: 0,
+               transition: {
+                    duration: 0.75, 
+                    ease: "easeOut",
+                    staggerChildren: 1, // numbers of second he wait to before animating the component one after another
+                    //when: 'afterChildren' // activate after the loading of all the children in the div selected
+               }
+          }
+          
+     }
+
      return(
         <About>
              <Description>
-                  <div className="title">
+                  <motion.div variants={container} initial="hidden" animate="show" className="title">
                        <Hide>
-                            <h2>We work to make</h2>
+                              <motion.h2 variants={titleAnim}>
+                                 We work to make
+                              </motion.h2>
                        </Hide>
                        <Hide>
-                            <h2>your <span>dreams</span> comes</h2>
+                              <motion.h2 variants={titleAnim}>
+                                 your <span>dreams</span> comes
+                              </motion.h2>
                        </Hide>
                        <Hide>
-                            <h2>true.</h2>
+                            <motion.h2 variants={titleAnim}>true.</motion.h2>
                        </Hide>
-                  </div>
+                  </motion.div>
                   <p>
                          Contact us for any photography or videography idea that you have.
                          We have professionals with amazing skills.
